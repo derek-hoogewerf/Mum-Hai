e8e071a8c059515a19a4a796a121803b6cf8f6f9
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-03-11 22:18:29 +0530

    added url for home page

--- mumhai/mumhai/urls.py
+++ mumhai/mumhai/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from home import views
+#from home import views
 from django.urls import path,include
 
 urlpatterns = [
-    path('/',include('home.urls')),
+    path('',include('home.urls')),
     path('admin/', admin.site.urls),
 ]
