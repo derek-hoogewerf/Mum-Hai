550bc98a5aedbc89d36dac8cfbb6729b20b1bc13
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-03-24 01:15:04 +0530

    dynamic content added

--- mumhai/home/views.py
+++ mumhai/home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Mum-Hai<h1>");
+    return render(request, 'home.html',{'name':'Derek'});
