ed81ebd020165e89366b14948cece751217c4ac5
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-03-24 19:40:30 +0530

    addded csvf token to encrypt data
    data has been encrypted

--- mumhai/home/views.py
+++ mumhai/home/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{'name':'Derek'});
+
+def add(request):
+    val1=int(request.POST['n1'])
+    val2=int(request.POST['n2'])
+    res=val1+val2
+
+    return render(request, 'result.html', {'result':res})
