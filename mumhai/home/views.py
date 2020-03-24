4a0ff509a4a25031bd2ba0d224b28459670cccf3
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-03-24 21:43:35 +0530

    static files added

--- mumhai/home/views.py
+++ mumhai/home/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html',{'name':'Derek'});
+"""def home(request):
+    return render(request, 'home.html',{'name':'Derek'})"""
 
 def add(request):
     val1=int(request.POST['n1'])
