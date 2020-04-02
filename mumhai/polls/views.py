a36d6f9dec9a33fd7edb74829747e01e5c52931f
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-04-02 17:07:28 +0530

    Got  oobject oor eeeeexcption error  handling

--- mumhai/polls/views.py
+++ mumhai/polls/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse, Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -13,11 +13,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question}
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
