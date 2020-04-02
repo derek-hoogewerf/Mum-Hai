445de14ce994e44d2c36a868e510832603fa1df1
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-04-02 17:16:38 +0530

    ..

--- mumhai/polls/views.py
+++ mumhai/polls/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect #,Http404
+#from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
