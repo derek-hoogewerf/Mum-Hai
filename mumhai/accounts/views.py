549f960dc3c6a34eb3673c6bdaf65c1e009aceb8
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-04-09 20:16:02 +0530

    Update views.py

--- mumhai/accounts/views.py
+++ mumhai/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -11,12 +12,14 @@ def register(request):
         email = request.POST['email'];
 
         if User.objects.filter(username=username).exists():
-            print ('User name')
+            messages.info(request,'User name')
+            return redirect('register')
         elif User.objects.filter(email=email).exists:
-            print ('email already registered')
+            messages.imfo(request,'email already registered')
+            return redirect('register')
         else:
             user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-            user.save();
+            user.save()
             print('user created')
             return redirect('/')
     else:
