3209b91e5715ee8513965445046e4308b3b3be5d
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-04-10 00:09:17 +0530

    Current (#29)
    * updated index.html
    
    * Update models.cpython-38.pyc
    
    * Update views.py
    
    * Update views.cpython-38.pyc
    
    * Update README.md
    
    * created app accounts
    
    used for registration
    
    * imported data from database
    
    destinations
    
    * created register page
    
    kept in templates
    
    * Update index.html
    
    added home button on logo
    
    * Update register.html
    
    * Update register.html
    
    * Added equirements.txt
    
    * Updated admin.py
    
    * Update admin.cpython-38.pyc
    
    * Update register.html
    
    * Update views.py
    
    * updated cache
    
    * added checks for email
    
    * Update register.html
    
    * Update views.py
    
    * Update views.cpython-38.pyc
    
    * Update views.py
    
    * Update register.html
    
    * Update views.cpython-38.pyc
    
    * Update views.py
    
    * Update views.cpython-38.pyc
    
    * Update register.html
    
    printing massages
    
    * Update views.py
    
    added messages module
    
    * Update views.cpython-38.pyc
    
    added user to database

---
+++ mumhai/accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+    if(request.method == 'POST'):
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Username taken')
+            return redirect('register')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request, 'email already registered')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+            user.save();
+            print('user created')
+            return redirect('/')
+    else:
+        return render(request, 'register.html')
--- mumhai/mumhai/urls.py
+++ mumhai/mumhai/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('',include('home.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
