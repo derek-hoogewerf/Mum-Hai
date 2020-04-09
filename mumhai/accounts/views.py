0500e42da71b838af2dc136adc4709c65aacd6e4
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-04-09 23:56:57 +0530

    Update views.py

--- mumhai/accounts/views.py
+++ mumhai/accounts/views.py
@@ -14,7 +14,7 @@ def register(request):
         if User.objects.filter(username=username).exists():
             messages.info(request, 'User name')
             return redirect('register')
-        elif User.objects.filter(email=email).exists:
+        elif User.objects.filter(email=email).exists():
             messages.info(request, 'email already registered')
             return redirect('register')
         else:
