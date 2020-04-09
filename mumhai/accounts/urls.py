01f6f8403dc0f5a2b15dac592c6d1cf244133426
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-04-10 00:24:37 +0530

    Update urls.py
    added login url

--- mumhai/accounts/urls.py
+++ mumhai/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('register',views.register, name='register'),                            #or use '/' depending on version
+    path("login", views.login, name="login")
 ]
