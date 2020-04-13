f4f1b7a13110cc7e225d4a9e2ead907898eb1831
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-04-13 23:37:55 +0530

    Update views.py

--- mumhai/accounts/views.py
+++ mumhai/accounts/views.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 # Create your views here.
+def login(request):
+    if request.method == 'post':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth,login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'invalid credentials')
+            return redirect('login')
+    else:
+        pass
+
+
 def register(request):
     if(request.method == 'POST'):
         first_name = request.POST['first_name']
@@ -21,6 +36,6 @@ def register(request):
             user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
             user.save();
             print('user created')
-            return redirect('/')
+            return redirect('login')
     else:
         return render(request, 'register.html')
